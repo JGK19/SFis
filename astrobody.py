e8f69a97a53619271d0f7f5707254c0d6d7df79c
JGK19 <106597685+JGK19@users.noreply.github.com>
2025-08-31 21:31:02 -0300

    debug: temp

--- astrobody.py
+++ astrobody.py
@@ -60,8 +60,10 @@ class AstroBody:
         x = self.pos[0]
         y = self.pos[1]
         pygame.draw.circle(screen, self.color, (x, y), self.radius)
-        draw_vector(screen, (x, y), self.force)
+        draw_vector(screen, (x, y), self.force, color=(255,0,0))
         draw_vector(screen, (x, y), self.vel, color=(0,255,0))
+        draw_vector(screen, (x, y), self.acc, color=(255,255,0))
+
     
     def update(self, delta_t):
         self.pos += (self.vel * delta_t)
